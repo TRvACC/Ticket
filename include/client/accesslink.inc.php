9bdd1a802382477b43456835aa3b2eda9f7f9f47
cptsnd <cptsnd@gmail.com>
2023-03-19 14:22:03 +0300

    Uploaded files

---
+++ include/client/accesslink.inc.php
@@ -0,0 +1,66 @@
+<?php
+if(!defined('OSTCLIENTINC')) die('Access Denied');
+
+$email=Format::input($_POST['lemail']?$_POST['lemail']:$_GET['e']);
+$ticketid=Format::input($_POST['lticket']?$_POST['lticket']:$_GET['t']);
+
+if ($cfg->isClientEmailVerificationRequired())
+    $button = __("Email Access Link");
+else
+    $button = __("View Ticket");
+?>
+<h1><?php echo __('Check Ticket Status'); ?></h1>
+<p><?php
+echo __('Please provide your email address and a ticket number.');
+if ($cfg->isClientEmailVerificationRequired())
+    echo ' '.__('An access link will be emailed to you.');
+else
+    echo ' '.__('This will sign you in to view your ticket.');
+?></p>
+<form action="login.php" method="post" id="clientLogin">
+    <?php csrf_token(); ?>
+<div style="display:table-row">
+    <div class="login-box">
+    <div><strong><?php echo Format::htmlchars($errors['login']); ?></strong></div>
+    <div>
+        <label for="email"><?php echo __('Email Address'); ?>:
+        <input id="email" placeholder="<?php echo __('e.g. john.doe@osticket.com'); ?>" type="text"
+            name="lemail" size="30" value="<?php echo $email; ?>" class="nowarn"></label>
+    </div>
+    <div>
+        <label for="ticketno"><?php echo __('Ticket Number'); ?>:
+        <input id="ticketno" type="text" name="lticket" placeholder="<?php echo __('e.g. 051243'); ?>"
+            size="30" value="<?php echo $ticketid; ?>" class="nowarn"></label>
+    </div>
+    <p>
+        <input class="btn" type="submit" value="<?php echo $button; ?>">
+    </p>
+    </div>
+    <div class="instructions">
+<div style="display:table-row">
+<?php
+
+$ext_bks = array();
+foreach (UserAuthenticationBackend::allRegistered() as $bk)
+    if ($bk instanceof ExternalAuthentication)
+        $ext_bks[] = $bk;
+
+if (count($ext_bks)) {
+    foreach ($ext_bks as $bk) { ?>
+<div class="external-auth"><?php $bk->renderExternalLink(); ?></div><?php
+    }
+}
+if ($cfg && $cfg->isClientRegistrationEnabled()) {
+    if (count($ext_bks)) echo '<hr style="width:70%"/>'; ?>
+    <div style="margin-bottom: 5px">
+    <?php echo __('Not yet registered?'); ?> <a href="account.php?do=create"><?php echo __('Create an account'); ?></a>
+    </div>
+<?php } ?>
+    <div><br />
+    <p><b><?php echo __("I'm a staff member"); ?></b> —
+    <a href="<?php echo ROOT_PATH; ?>scp/"><?php echo __('sign in here'); ?></a></p>
+    </div>
+    </div>
+    </div>
+</div>
+</form>
---
+++ include/client/login.inc.php
@@ -0,0 +1,47 @@
+<?php
+if(!defined('OSTCLIENTINC')) die('Access Denied');
+
+$email=Format::input($_POST['luser']?:$_GET['e']);
+$passwd=Format::input($_POST['lpasswd']?:$_GET['t']);
+
+$content = Page::lookupByType('banner-client');
+
+if ($content) {
+    list($title, $body) = $ost->replaceTemplateVariables(
+        array($content->getLocalName(), $content->getLocalBody()));
+} else {
+    $title = __('Sign In');
+    $body = __('To better serve you, we encourage our clients to register for an account and verify the email address we have on record.');
+}
+
+?>
+<h1><?php echo Format::display($title); ?></h1>
+<p><?php echo Format::display($body); ?></p>
+<form action="login.php" method="post" id="clientLogin">
+    <?php csrf_token(); ?>
+<div style="display:table-row">
+<?php
+
+$ext_bks = array();
+foreach (UserAuthenticationBackend::allRegistered() as $bk)
+    if ($bk instanceof ExternalAuthentication)
+        $ext_bks[] = $bk;
+
+if (count($ext_bks)) {
+    foreach ($ext_bks as $bk) { ?>
+<div class="external-auth"><?php $bk->renderExternalLink(); ?></div><?php
+    }
+}
+if ($cfg && $cfg->isClientRegistrationEnabled()) {
+    if (count($ext_bks)) echo '<hr style="width:70%"/>'; ?>
+    <div style="margin-bottom: 5px">
+    <?php echo __('Not yet registered?'); ?> <a href="account.php?do=create"><?php echo __('Create an account'); ?></a>
+    </div>
+<?php } ?>
+    <div><br />
+    <p><b><?php echo __("I'm a staff member"); ?></b> —
+    <a href="<?php echo ROOT_PATH; ?>scp/"><?php echo __('sign in here'); ?></a></p>
+    </div>
+    </div>
+</div>
+</form>
